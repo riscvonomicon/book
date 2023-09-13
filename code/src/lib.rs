fa1fb661ee86963e874161a1558586b687d63cf5
Gijs Burghoorn <g.burghoorn@gmail.com>
2023-09-13 20:27:47 +0200

    Update: conditionally implement zk

--- code/src/lib.rs
+++ code/src/lib.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![warn(clippy::undocumented_unsafe_blocks)]
 #![deny(unsafe_op_in_unsafe_fn)]
-#![feature(stdsimd, riscv_ext_intrinsics, riscv_target_feature)]
+#![cfg_attr(
+    any(target_arch = "riscv32", target_arch = "riscv64"),
+    feature(stdsimd, riscv_ext_intrinsics, riscv_target_feature)
+)]
 
 pub mod zk;
--- code/src/zk/mod.rs
+++ code/src/zk/mod.rs
@@ -1,4 +1,5 @@
 //! Many of these examples are based of:
 //! https://github.com/rvkrypto/rvkrypto-fips
 
+#[cfg(target_arch = "riscv32")]
 pub mod aes32;
